bf72f08f3012932f7f7045f8adbfbcd19a5b9872
troubleman96 <itslugenge96@gmail.com>
2025-04-24 11:52:13 +0300

    V1

---
+++ .history/basics_20250424083313.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine, text
+
+engine  = create_engine('sqlite:///mydatabase.db', echo=True)
+
+conn = engine.connect()
+
+conn.execute(text("CREATE TABLE IF NOT EXISTS people (name TEXT, age INTERGER)"))
+
+conn.commit()
+
+
---
+++ basics.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine, text
+
+engine  = create_engine('sqlite:///mydatabase.db', echo=True)
+
+conn = engine.connect()
+
+conn.execute(text("CREATE TABLE IF NOT EXISTS people (name TEXT, age INTERGER)"))
+
+conn.commit()
+
+
+from sqlalchemy .orm import Session
+
+session = Session(engine)
+
+session.execute(text('INSERT INTO people (name,age) VALUES ("troubleman", 24);'))
+session.commit()
